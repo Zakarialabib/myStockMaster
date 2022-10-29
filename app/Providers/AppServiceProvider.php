bcd3e4a0da180953b58346f820e2c6b477bf931d
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2022-10-29 21:06:18 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
 
         Model::shouldBeStrict(! $this->app->isProduction());
