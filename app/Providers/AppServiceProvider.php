b00031464328b904853fff7e3790fb794a850f25
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2022-12-19 13:13:15 +0100

    add pint.yml action to auto apply formating v4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -47,14 +47,18 @@ class AppServiceProvider extends ServiceProvider
     /** @return \App\Models\Language|\Illuminate\Database\Eloquent\Model|array|null */
     private function getLanguages()
     {
-        if ( ! Schema::hasTable('languages')) {
-            return;
-        }
+        if(!app()->runningInConsole())
+        {
 
-        return cache()->rememberForever('languages', function () {
-            return Session::has('language')
-                ? Language::pluck('name', 'code')->toArray()
-                : Language::where('is_default', 1)->first();
-        });
+            if ( ! Schema::hasTable('languages')) {
+                return;
+            }
+
+            return cache()->rememberForever('languages', function () {
+                return Session::has('language')
+                    ? Language::pluck('name', 'code')->toArray()
+                    : Language::where('is_default', 1)->first();
+            });
+        }
     }
 }
