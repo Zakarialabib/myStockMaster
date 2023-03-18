7c1876216615787a4430a7518009dcb8b8e2408e
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-18 12:42:00 +0100

    missing Str in Jobs

--- app/Jobs/SyncYouCanProducts.php
+++ app/Jobs/SyncYouCanProducts.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Throwable;
 
 class SyncYouCanProducts implements ShouldQueue
