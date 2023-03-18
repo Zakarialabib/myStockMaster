9d7412a55f4e0b5050d4925b9bcd8cc7118d220b
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-18 12:42:00 +0100

    missing Str in Jobs

--- app/Jobs/SyncCustomProducts.php
+++ app/Jobs/SyncCustomProducts.php
@@ -11,6 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
 
 class SyncCustomProducts implements ShouldQueue
 {
