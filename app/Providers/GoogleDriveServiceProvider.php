cb7ef037e58a52c19129b7c030be2f62ad87c86f
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-27 14:34:24 +0000

    Enabling Gdrive backup

---
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+// use Masbug\Flysystem\GoogleDrive\GoogleDriveAdapter;
+use Masbug\Flysystem\GoogleDriveAdapter;
+
+class GoogleDriveServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \Storage::extend('google', function($app, $config) {
+            $client = new \Google_Client();
+            $client->setClientId($config['clientId']);
+            $client->setClientSecret($config['clientSecret']);
+            $client->refreshToken($config['refreshToken']);
+            $service = new \Google_Service_Drive($client);
+
+            $options = [];
+            if(isset($config['teamDriveId'])) {
+                $options['teamDriveId'] = $config['teamDriveId'];
+            }
+
+            $adapter = new GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
+            $driver = new \League\Flysystem\Filesystem($adapter);
+            return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
+        });
+    }    
+}
