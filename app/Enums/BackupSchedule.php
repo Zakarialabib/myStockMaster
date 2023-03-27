f46d6e294c2f49ca88a73084f8e19f40ce3d9b68
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-27 16:06:58 +0000

    cleaning enums

--- app/Enums/BackupSchedule.php
+++ app/Enums/BackupSchedule.php
@@ -12,11 +12,4 @@ enum BackupSchedule: string
 
     case MONTHLY = '2';
 
-
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'name', 'value');
-    }
-
-
 }
--- app/Enums/SaleStatus.php
+++ app/Enums/SaleStatus.php
@@ -18,14 +18,4 @@ enum SaleStatus: string
 
     case CANCELED = '5';
 
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'name', 'value');
-    }
-
-    // loop through the values:
-
-    // @foreach(App\Enums\PaymentStatus::values() as $key=>$value)
-    //     <option value="{{ $key }}">{{ $value }}</option>
-    // @endforeach
 }
