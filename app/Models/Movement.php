b098700322a1eda69f5006e9c27dc1f976f9c8ee
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-04-01 13:28:04 +0000

    Tracking product movements

---
+++ app/Models/Movement.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'quantity',
+        'price',
+        'date',
+        'movable_id',
+        'movable_type',
+        'user_id',
+    ];
+
+    public function movable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 /**
  * App\Models\Product
@@ -72,6 +73,8 @@ use Illuminate\Notifications\Notifiable;
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereUuid($value)
  * @method static \Database\Factories\ProductFactory factory($count = null, $state = [])
+ * @property int $featured
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereFeatured($value)
  * @mixin \Eloquent
  */
 class Product extends Model
@@ -128,7 +131,7 @@ class Product extends Model
     public function __construct(array $attributes = [])
     {
         $this->setRawAttributes([
-            'code' => Carbon::now()->format('Y-m-d').mt_rand(10000000, 99999999),
+            'code' => Carbon::now()->format('Y-m-d') . mt_rand(10000000, 99999999),
         ], true);
         parent::__construct($attributes);
     }
@@ -141,6 +144,11 @@ class Product extends Model
     public function brand(): BelongsTo
     {
         return $this->belongsTo(Brand::class, 'brand_id', 'id');
+    }
+
+    public function movements(): MorphMany
+    {
+        return $this->morphMany(Movement::class, 'movable');
     }
 
     /**
@@ -168,4 +176,6 @@ class Product extends Model
             set: fn ($value) => $value * 100,
         );
     }
+
+  
 }
