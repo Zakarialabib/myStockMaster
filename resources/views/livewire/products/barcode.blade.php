3172b164b80782227a07b8f3aa84927f527d34d3
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-04-16 22:24:55 +0000

    Update barcode.blade.php

--- resources/views/livewire/products/barcode.blade.php
+++ resources/views/livewire/products/barcode.blade.php
@@ -8,20 +8,38 @@
                     <x-slot name="thead">
                         <x-table.th>{{ __('Product Name') }}</x-table.th>
                         <x-table.th>{{ __('Code') }}</x-table.th>
+                        <x-table.th>{{ __('Size') }}</x-table.th>
                         <x-table.th>
                             {{ __('Quantity') }} <i class="bi bi-question-circle-fill text-info" data-toggle="tooltip"
                                 data-placement="top" title="Max Quantity: 100"></i>
                         </x-table.th>
+                        <x-table.th></x-table.th>
                     </x-slot>
                     <x-table.tbody>
                         @if (!empty($products))
                             @forelse ($products as $product)
-                                <x-table.tr wire:key="{{ $product['id'] }}" >
+                                <x-table.tr wire:key="{{ $product['id'] }}">
                                     <x-table.td>{{ $product['name'] }}</x-table.td>
                                     <x-table.td>{{ $product['code'] }}</x-table.td>
                                     <x-table.td style="width: 200px;">
-                                        <x-input wire:model="quantity" type="text" value="{{ $quantity }}"
-                                            required />
+                                        <x-input wire:model.lazy="quantity.{{ $product['id'] }}" type="text"
+                                            min="0" max="100" required />
+                                    </x-table.td>
+                                    <x-table.td>
+                                        <select name="barcodeSize" id="barcodeSize"
+                                            wire:model.lazy="barcodeSize.{{ $product['id'] }}">
+                                            <option value="1">{{ __('Small') }}</option>
+                                            <option value="2">{{ __('Medium') }}</option>
+                                            <option value="3">{{ __('Large') }}</option>
+                                            <option value="4">{{ __('Extra') }}</option>
+                                            <option value="5">{{ __('Huge') }}</option>
+                                        </select>
+                                    </x-table.td>
+                                    <x-table.td>
+                                        <button wire:click="deleteProduct({{ $product['id'] }})"
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                            {{ __('Delete') }}
+                                        </button>
                                     </x-table.td>
                                 </x-table.tr>
                             @empty
@@ -33,11 +51,10 @@
                     </x-table.tbody>
                 </x-table>
             </div>
-            <div class="flex justify-center mt-3">
-                <button wire:click="generateBarcodes({{ $product }}, {{ $quantity }})" type="button"
-                    class="block uppercase mx-auto shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">
+            <div class="flex justify-center text-center mt-3">
+                <x-button wire:click="generateBarcodes" type="button" primary class="w-full">
                     {{ __('Generate Barcodes') }}
-                </button>
+                </x-button>
             </div>
         </div>
     </div>
@@ -50,31 +67,26 @@
 
     @if (!empty($barcodes))
         <div class="text-center mb-3">
-            {{-- open print page with getPDF --}}
-            <button
-                class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest disabled:opacity-25 transition ease-in-out duration-150 bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300"
-                wire:click="getPdf" target="_blank" wire:loading.attr="disabled" type="button">
+            <x-button primary wire:click="downloadBarcodes" type="button">
                 {{ __('Download PDF') }}
-            </button>
+            </x-button>
         </div>
         <div class="card">
             <div class="p-4">
-                <div class="flex flex-wrap justify-center">
+                <div class="flex flex-wrap justify-center border-collapse">
                     @foreach ($barcodes as $barcode)
-                        @foreach ($products as $product)
-                            <div class="lg:w-1/3 md:w-1/4 sm:w-1/2"
-                                style="border: 1px solid #ffffff;border-style: dashed;">
-                                <p class="text-black font-bold text-lg text-center my-2">
-                                    {{ $product->name }}
-                                </p>
-                                <div>
-                                    {!! $barcode !!}
-                                </div>
-                                <p class="text-black font-bold text-lg text-center my-2">
-                                    {{ __('Price') }} : {{ $product->price }}
-                                </p>
+                        <div class="lg:w-1/3 md:w-1/4 sm:w-1/2"
+                            style="border: 1px solid #ffffff; border-style: dashed;">
+                            <p class="text-black font-bold text-lg text-center my-2">
+                                {{ $barcode['name'] }}
+                            </p>
+                            <div class="flex justify-center">
+                                {!! $barcode['barcode'] !!}
                             </div>
-                        @endforeach
+                            <p class="text-black font-bold text-lg text-center my-2">
+                                {{ __('Price') }}: {{ $barcode['price'] }}
+                            </p>
+                        </div>
                     @endforeach
                 </div>
             </div>
