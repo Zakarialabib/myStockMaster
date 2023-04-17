6c6dbbd0285b1d8ff5a5019fc69f957119714a39
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-04-17 00:29:29 +0000

    improving system update

--- app/Helpers/GitHandler.php
+++ app/Helpers/GitHandler.php
@@ -7,54 +7,47 @@ namespace App\Helpers;
 class GitHandler
 {
     private $message;
-    
-    public function fetchAndPull()
+
+    public function checkForUpdates()
     {
         $branch = env('GIT_BRANCH', 'master');
-
         exec("git fetch origin $branch", $output, $return);
 
         if ($return === 0) {
-            $this->message = "Fetched updates from origin/$branch.";
-            exec("git reset --hard FETCH_HEAD", $output, $return);
-            if ($return === 0) {
-                $this->message = "Hard reset to latest updates from origin/$branch.";
-                exec("git clean -df", $output, $return);
-                if ($return === 0) {
-                    $this->message = "Cleaned up untracked files and directories.";
-                } else {
-                    $this->message = "Error cleaning untracked files and directories.";
-                }
+            exec("git rev-parse HEAD", $localHead, $return);
+            exec("git rev-parse FETCH_HEAD", $remoteHead, $return);
+
+            if ($localHead !== $remoteHead) {
+                $this->message = "Updates available on origin/$branch.";
+                return true;
             } else {
-                $this->message = "Error resetting to latest updates from origin/$branch.";
+                $this->message = "No updates available.";
+                return false;
             }
         } else {
-            $this->message = "__('Error fetching updates from origin')./$branch.";
+            $this->message = "Error fetching updates from origin/$branch.";
+            return false;
         }
+    }
 
+    public function fetchAndPull()
+    {
+        $branch = env('GIT_BRANCH', 'master');
+        exec("git fetch origin $branch", $output, $return);
+    
+        if ($return === 0) {
+            $this->message = "Fetched updates from origin/$branch.";
+            exec("git merge origin/$branch", $output, $return);
+            if ($return === 0) {
+                $this->message = "Merged updates from origin/$branch.";
+            } else {
+                $this->message = "Error merging updates from origin/$branch.";
+            }
+        } else {
+            $this->message = "Error fetching updates from origin/$branch.";
+        }
+    
         return $this->message;
     }
-
-    public function pushChanges()       
-    {
-        exec("git diff --exit-code", $diff, $return);
-
-        if ($return === 0) {
-            $this->message = "No changes to push.";
-            return;
-        } 
-        // Get the repository URL and branch from the .env file
-        $repoUrl = env('REPO_URL');
-        $repoBranch = env('REPO_BRANCH');
-        
-        // Push the changes to the remote repository
-        exec("git push $repoUrl $repoBranch", $output, $return);
-        if ($return=== 0) {
-            $this->message = __("Changes pushed successfully.");
-            return;
-        } 
-            
-        $this->message = __("Error pushing changes.");
-
-    }
+    
 }
