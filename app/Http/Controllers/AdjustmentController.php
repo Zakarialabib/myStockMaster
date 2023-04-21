5cc9bbb96adcb545a9314dd7bfa8d3966abf0b3d
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-04-21 17:25:15 +0000

    moving to livewire & adding multiWarehouse logic

--- app/Http/Controllers/AdjustmentController.php
+++ app/Http/Controllers/AdjustmentController.php
@@ -28,51 +28,6 @@ class AdjustmentController extends Controller
         return view('admin.adjustment.create');
     }
 
-    public function store(Request $request)
-    {
-        abort_if(Gate::denies('adjustment_create'), 403);
-
-        $request->validate([
-            'reference'   => 'required|string|max:255',
-            'date'        => 'required|date',
-            'note'        => 'nullable|string|max:1000',
-            'product_ids' => 'required',
-            'quantities'  => 'required',
-            'types'       => 'required',
-        ]);
-
-        DB::transaction(function () use ($request) {
-            $adjustment = Adjustment::create([
-                'date' => $request->date,
-                'note' => $request->note,
-            ]);
-
-            foreach ($request->product_ids as $key => $id) {
-                AdjustedProduct::create([
-                    'adjustment_id' => $adjustment->id,
-                    'product_id'    => $id,
-                    // 'warehouse_id'    => $id,
-                    'quantity' => $request->quantities[$key],
-                    'type'     => $request->types[$key],
-                ]);
-
-                $product = Product::findOrFail($id);
-
-                if ($request->types[$key] === 'add') {
-                    $product->update([
-                        'quantity' => $product->quantity + $request->quantities[$key],
-                    ]);
-                } elseif ($request->types[$key] === 'sub') {
-                    $product->update([
-                        'quantity' => $product->quantity - $request->quantities[$key],
-                    ]);
-                }
-            }
-        });
-
-        return redirect()->route('adjustments.index');
-    }
-
     public function edit(Adjustment $adjustment)
     {
         abort_if(Gate::denies('adjustment_edit'), 403);
@@ -80,64 +35,6 @@ class AdjustmentController extends Controller
         return view('admin.adjustment.edit', compact('adjustment'));
     }
 
-    public function update(Request $request, Adjustment $adjustment)
-    {
-        abort_if(Gate::denies('adjustment_edit'), 403);
-
-        $request->validate([
-            'reference'   => 'required|string|max:255',
-            'date'        => 'required|date',
-            'note'        => 'nullable|string|max:1000',
-            'product_ids' => 'required',
-            'quantities'  => 'required',
-            'types'       => 'required',
-        ]);
-
-        DB::transaction(function () use ($request, $adjustment) {
-            $adjustment->update([
-                'reference' => $request->reference,
-                'date'      => $request->date,
-                'note'      => $request->note,
-            ]);
-
-            foreach ($adjustment->adjustedProducts as $adjustedProduct) {
-                $product = Product::findOrFail($adjustedProduct->product?->id);
-
-                if ($adjustedProduct->type === 'add') {
-                    $product->update([
-                        'quantity' => $product->quantity - $adjustedProduct->quantity,
-                    ]);
-                } elseif ($adjustedProduct->type === 'sub') {
-                    $product->update([
-                        'quantity' => $product->quantity + $adjustedProduct->quantity,
-                    ]);
-                }
-
-                $adjustedProduct->delete();
-            }
-
-            foreach ($request->product_ids as $key => $id) {
-                AdjustedProduct::create([
-                    'adjustment_id' => $adjustment->id,
-                    'product_id'    => $id,
-                    'quantity'      => $request->quantities[$key],
-                    'type'          => $request->types[$key],
-                ]);
-
-                $product = Product::findOrFail($id);
-
-                if ($request->types[$key] === 'add') {
-                    $product->update([
-                        'quantity' => $product->quantity + $request->quantities[$key],
-                    ]);
-                } elseif ($request->types[$key] === 'sub') {
-                    $product->update([
-                        'quantity' => $product->quantity - $request->quantities[$key],
-                    ]);
-                }
-            }
-        });
-
-        return redirect()->route('adjustments.index');
-    }
+  
+    
 }
---
+++ resources/views/livewire/adjustment/edit.blade.php
@@ -0,0 +1,81 @@
+<div>
+    <div class="p-4">
+        <x-validation-errors class="mb-4" :errors="$errors" />
+
+        <form wire:submit.prevent="update">
+            <div class="flex flex-wrap -mx-2 mb-3">
+                <div class="xl:w-1/2 lg:w-1/2 sm:w-full px-3">
+                    <x-label for="reference" :value="__('Reference')" required />
+                    <x-input type="text" wire:model.lazy="reference" name="reference" required disabled />
+                </div>
+                <div class="xl:w-1/2 lg:w-1/2 sm:w-full px-3">
+                    <x-label for="date" :value="__('Date')" required />
+                    <p>{{ $this->date }}</p>
+                </div>
+            </div>
+
+            <div class="table-responsive">
+                <x-table>
+                    <x-slot name="thead">
+                        <x-table.th>#</x-table.th>
+                        <x-table.th>{{ __('Product Name') }}</x-table.th>
+                        <x-table.th>{{ __('Stock') }}</x-table.th>
+                        <x-table.th>{{ __('Quantity') }}</x-table.th>
+                        <x-table.th>{{ __('Type') }}</x-table.th>
+                        <x-table.th>{{ __('Action') }}</x-table.th>
+                    </x-slot>
+                    <x-table.tbody>
+                        @foreach ($products as $key => $product)
+                            <x-table.tr wire:loading.class.delay="opacity-50">
+                                <x-table.td>{{ $key + 1 }}</x-table.td>
+                                <x-table.td>{{ $product['product']['name'] }}
+                                    <small>{{ $product['product']['code'] }}</small>
+                                </x-table.td>
+                                <x-table.td>
+                                    <span class="badge badge-info">
+                                        {{ $product['product']['quantity'] }}
+                                        {{ $product['product']['unit'] }}
+                                    </span>
+                                </x-table.td>
+                                <x-table.td>
+                                    <input type="text" name="quantity" min="1"
+                                        wire:model="products.{{ $key }}.quantity"
+                                        class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md mt-1">
+                                </x-table.td>
+                                <x-table.td>
+                                    <select name="types[]" wire:model.lazy="products.{{ $key }}.type"
+                                        class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md mt-1">
+                                        <option value="add"
+                                            {{ isset($product['type']) && $product['type'] == 'add' ? 'selected' : '' }}>
+                                            (+) {{ __('Addition') }}</option>
+                                        <option value="sub"
+                                            {{ isset($product['type']) && $product['type'] == 'sub' ? 'selected' : '' }}>
+                                            (-) {{ __('Subtraction') }}</option>
+                                    </select>
+                                </x-table.td>
+                                <x-table.td>
+                                    <button type="button" class="btn btn-danger"
+                                        wire:click="removeProduct({{ $key }})">
+                                        <i class="bi bi-trash"></i>
+                                    </button>
+                                </x-table.td>
+                            </x-table.tr>
+                        @endforeach
+
+                    </x-table.tbody>
+                </x-table>
+            </div>
+
+            <div class="mb-4">
+                <x-label for="note" :value="__('Note (If Needed)')"  />
+                <textarea name="note" id="note" rows="5" wire:model.lazy="note"
+                    class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md mt-1"></textarea>
+            </div>
+            <div class="mt-3">
+                <x-button type="submit" primary>
+                    {{ __('Edit Adjustment') }}
+                </x-button>
+            </div>
+        </form>
+    </div>
+</div>
