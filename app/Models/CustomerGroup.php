76ef7e16620fcca0cb4c5893ce0e5aa2fd55fcd6
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-08-26 14:56:07 +0100

    Create CustomerGroup.php

---
+++ app/Models/CustomerGroup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Support\HasAdvancedFilter;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CustomerGroup extends Model
+{
+    use HasAdvancedFilter;
+    use HasFactory;
+
+    public $orderable = [
+        'id', 'name', 'percentage', 'status'
+    ];
+
+    public $filterable = [
+        'id', 'name', 'percentage', 'status'
+    ];
+
+    protected $fillable =[
+        "name", "percentage", "status"
+    ];
+}
