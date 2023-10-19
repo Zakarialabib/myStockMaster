5bcb824a5a2ae9be4d70418b8604501fc750b377
Zakaria Labib <58374576+Zakarialabib@users.noreply.github.com>
2023-10-19 13:29:30 +0100

    adding NativePhp config

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\Window;
+use Native\Laravel\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open();
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+            'memory_limit' => '512M',
+            'display_errors' => '1',
+            'error_reporting' => 'E_ALL',
+            'max_execution_time' => '360',
+            'max_input_time' => '0',
+        ];
+    }
+}
---
+++ config/nativephp.php
@@ -0,0 +1,94 @@
+<?php
+
+return [
+    /**
+     * The version of your app.
+     * It is used to determine if the app needs to be updated.
+     * Increment this value every time you release a new version of your app.
+     */
+    'version' => env('NATIVEPHP_APP_VERSION', '1.0.0'),
+
+    /**
+     * The ID of your application. This should be a unique identifier
+     * usually in the form of a reverse domain name.
+     * For example: com.nativephp.app
+     */
+    'app_id' => env('NATIVEPHP_APP_ID'),
+
+    /**
+     * If your application allows deep linking, you can specify the scheme
+     * to use here. This is the scheme that will be used to open your
+     * application from within other applications.
+     * For example: "nativephp"
+     *
+     * This would allow you to open your application using a URL like:
+     * nativephp://some/path
+     */
+    'deeplink_scheme' => env('NATIVEPHP_DEEPLINK_SCHEME'),
+
+    /**
+     * The author of your application.
+     */
+    'author' => env('NATIVEPHP_APP_AUTHOR'),
+
+    /**
+     * The default service provider for your application. This provider
+     * takes care of bootstrapping your application and configuring
+     * any global hotkeys, menus, windows, etc.
+     */
+    'provider' => \App\Providers\NativeAppServiceProvider::class,
+
+    /**
+     * A list of environment keys that should be removed from the
+     * .env file when the application is bundled for production.
+     * You may use wildcards to match multiple keys.
+     */
+    'cleanup_env_keys' => [
+        'AWS_*',
+        'DO_SPACES_*',
+        '*_SECRET',
+        'NATIVEPHP_UPDATER_PATH',
+        'NATIVEPHP_APPLE_ID',
+        'NATIVEPHP_APPLE_ID_PASS',
+        'NATIVEPHP_APPLE_TEAM_ID',
+    ],
+
+    /**
+     * The NativePHP updater configuration.
+     */
+    'updater' => [
+        /**
+         * Whether or not the updater is enabled. Please note that the
+         * updater will only work when your application is bundled
+         * for production.
+         */
+        'enabled' => env('NATIVEPHP_UPDATER_ENABLED', true),
+
+        /**
+         * The updater provider to use.
+         * Supported: "s3", "spaces"
+         */
+        'default' => env('NATIVEPHP_UPDATER_PROVIDER', 'spaces'),
+
+        'providers' => [
+            's3' => [
+                'driver' => 's3',
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                'region' => env('AWS_DEFAULT_REGION'),
+                'bucket' => env('AWS_BUCKET'),
+                'endpoint' => env('AWS_ENDPOINT'),
+                'path' => env('NATIVEPHP_UPDATER_PATH', null),
+            ],
+
+            'spaces' => [
+                'driver' => 'spaces',
+                'key' => env('DO_SPACES_KEY_ID'),
+                'secret' => env('DO_SPACES_SECRET_ACCESS_KEY'),
+                'name' => env('DO_SPACES_NAME'),
+                'region' => env('DO_SPACES_REGION'),
+                'path' => env('NATIVEPHP_UPDATER_PATH', null),
+            ],
+        ],
+    ],
+];
