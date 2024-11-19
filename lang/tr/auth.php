036a9ebc1a729ec665fe144d525a921860a5cb4c
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-19 23:55:26 +0300

    Create auth.php

---
+++ lang/tr/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Kimlik Doğrulama Dil Satırları
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki dil satırları, kimlik doğrulama sırasında kullanıcıya çeşitli
+    | mesajlar göstermek için kullanılır. Bu dil satırlarını, uygulamanızın
+    | gereksinimlerine göre özgürce düzenleyebilirsiniz.
+    |
+    */
+
+    'failed'   => 'Bu kimlik bilgileri kayıtlarımızla eşleşmiyor.',
+    'password' => 'Girilen parola hatalı.',
+    'throttle' => 'Çok fazla giriş denemesi yapıldı. Lütfen :seconds saniye sonra tekrar deneyin.',
+
+];
