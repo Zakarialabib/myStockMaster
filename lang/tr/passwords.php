096f4243b0f1d50454feb4781bc14d4276c7d466
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-19 23:58:02 +0300

    Create passwords.php

---
+++ lang/tr/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Şifre Sıfırlama Dil Satırları
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki dil satırları, şifre sıfırlama girişimi sırasında geçersiz bir
+    | token ya da geçersiz yeni şifre gibi nedenlerle başarısız olan durumlarda
+    | kullanılan varsayılan mesajlardır.
+    |
+    */
+
+    'reset'     => 'Şifreniz sıfırlandı!',
+    'sent'      => 'Şifre sıfırlama bağlantısını e-posta ile gönderdik!',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token'     => 'Bu şifre sıfırlama belirteci geçersiz.',
+    'user'      => "Bu e-posta adresine sahip bir kullanıcı bulunamadı.",
+
+];
