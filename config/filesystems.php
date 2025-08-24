064bd29763d6f6f787b0286b5ce466f6f794c5a9
Zakaria Labib <58374576+Zakarialabib@users.noreply.github.com>
2025-08-25 00:08:34 +0100

    refactor: remove google drive backup functionality
    Remove all Google Drive related backup code including configuration, UI elements, translations and methods. This feature was not being used and was adding unnecessary complexity to the codebase.

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,15 +63,6 @@ return [
             'throw' => false,
         ],
 
-        'google' => [
-            'driver' => 'google',
-            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
-            'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
-            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
-            'folder' => env('GOOGLE_DRIVE_BACKUP_FOLDER'), // without folder is root of drive or team drive
-            //'teamDriveId' => env('GOOGLE_DRIVE_TEAM_DRIVE_ID'),
-        ],
-
     ],
 
     /*
