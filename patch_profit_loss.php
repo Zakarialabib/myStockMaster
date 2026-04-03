995be1ef61a0bb21931149f68161bdb54c3658fd
Zakaria Labib <zakarialabib@gmail.com>
2026-04-03 21:19:50 +0100

    delete - automated upgrade of code

--- patch_profit_loss.php
+++
@@ -1,6 +0,0 @@
-<?php
-$content = file_get_contents('app/Livewire/Reports/ProfitLossReport.php');
-$content = str_replace('public $warehouses;', '', $content);
-$content = preg_replace('/\$this->warehouses = [^;]+;/', '', $content);
-$content = str_replace('public function render()', "#[Computed]\n    public function warehouses()\n    {\n        return Warehouse::pluck('name', 'id')->toArray();\n    }\n\n    public function render()", $content);
-file_put_contents('app/Livewire/Reports/ProfitLossReport.php', $content);
--- patch_purchases.php
+++
@@ -1,7 +0,0 @@
-<?php
-$content = file_get_contents('app/Livewire/Reports/PurchasesReport.php');
-$content = str_replace('public $suppliers;', '', $content);
-$content = preg_replace('/\$this->suppliers = [^;]+;/', '', $content);
-$content = preg_replace('/public function mount\(\): void\s*\{/', "public function mount(): void\n    {", $content);
-$content = str_replace('public function render()', "#[Computed]\n    public function suppliers()\n    {\n        return Supplier::select(['id', 'name'])->get();\n    }\n\n    public function render()", $content);
-file_put_contents('app/Livewire/Reports/PurchasesReport.php', $content);
